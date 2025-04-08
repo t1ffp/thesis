884869fe7ecd3d4d439d6341429008edca871abd
Tiff ☆彡 <pengt796@newschool.edu>
2025-04-08 15:30:48 -0400

    main menu done kind of

---
+++ prototype/Assets/Scripts/MainMenuInputs.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuInputs : MonoBehaviour
+{
+    //private bool canClick = false;
+
+    public Image flashImage; 
+    public float flashDuration = 1.5f;
+
+    private bool isTransitioning = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isTransitioning) return;
+
+        if (other.CompareTag("Start") && Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(LoadScene());
+        }
+        if (other.CompareTag("Exit") && Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("exit");
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isTransitioning) return;
+
+        if (other.CompareTag("Start") && Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(LoadScene());
+        }
+        if (other.CompareTag("Exit") && Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("exit");
+            Application.Quit();
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        isTransitioning = true;
+
+        float elapsed = 0f;
+        Color color = flashImage.color;
+
+        while (elapsed < flashDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsed / flashDuration);
+            flashImage.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.3f);
+
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    /**
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && canClick)
+            {
+                //Debug.Log("hit");
+                int currentIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentIndex + 1);
+            }
+
+        }
+    **/
+
+}
---
+++ prototype/Assets/Scripts/TextAnim.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextAnim : MonoBehaviour
+{
+    public Animator animator;
+    //private bool canClick = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            //Debug.Log("entered");
+            animator.SetBool("Play", true);
+            //canClick = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            animator.SetBool("Play", true);
+            //canClick = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Light"))
+        {
+            //Debug.Log("entered");
+            animator.SetBool("Play", false);
+            //canClick = false;
+        }
+    }
+
+/**
+    private void OnMouseDown()
+    {
+        if (canClick)
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene + 1);
+        }
+    }
+**/
+
+}
